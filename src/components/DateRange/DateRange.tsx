52fbfccb65102925963d69eaf2646f2b84a789fc
Victor Chepkasov <capykasa@gmail.com>
2025-02-24 19:42:38 +0500

    добавление анимации для пагинации

--- src/components/DateRange/DateRange.tsx
+++ src/components/DateRange/DateRange.tsx
@@ -1,6 +1,10 @@
 import styles from './DateRange.module.scss'
 import { Dispatcher } from '@/types/Dispatcher'
 import { HistoricalDate } from '@/types/HistoricalDate'
+import { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { findDistanceOfArrayItems } from '@/utils'
+import { useGSAP } from '@gsap/react'
 
 interface DateRangeProps {
   dates: HistoricalDate[]
@@ -8,18 +12,96 @@ interface DateRangeProps {
   setDatePage: Dispatcher<number>
 }
 
+const basicRotate = 30
+
 export const DateRange = ({ dates, datePage, setDatePage }: DateRangeProps) => {
+  const datePages = Object.keys(dates).map(item => Number(item))
+  const degStep = 360 / datePages.length
+  const startDeg = basicRotate - degStep * datePage
+
+  const currentEventsPage = dates[datePage]
+
+  const [roundRotate, setRoundRotate] = useState(startDeg)
+  const [activePage, setActivePage] = useState(datePage)
+  const [dateRangeStart, setDateRangeStart] = useState(currentEventsPage?.events[0].date)
+  const [dateRangeEnd, setDateRangeEnd] = useState(currentEventsPage?.events[currentEventsPage.events.length - 1].date)
+
+  const rangeListRef = useRef<HTMLDivElement>(null)
+  const dateStartRef = useRef<HTMLSpanElement>(null)
+  const dateEndRef = useRef<HTMLSpanElement>(null)
+
+  useEffect(() => {
+    rangeListRef.current.style.rotate = `${startDeg}deg`
+  }, [])
+
+  useGSAP(() => {
+    const newDateStart = dates[datePage].events[0].date
+    const newDateEnd = dates[datePage].events[dates[datePage].events.length - 1].date
+
+    gsap.to(dateStartRef.current, {
+      textContent: `+=${newDateStart - dateRangeStart}`,
+      roundProps: 'textContent',
+      onUpdate: () => setDateRangeStart(newDateStart),
+    })
+    gsap.to(dateEndRef.current, {
+      textContent: `+=${newDateEnd - dateRangeEnd}`,
+      roundProps: 'textContent',
+      onUpdate: () => setDateRangeEnd(newDateEnd),
+    })
+  }, [datePage])
+
+  useGSAP(() => {
+    const pageDiffArr = findDistanceOfArrayItems(datePages, activePage, datePage)
+
+    let deg = degStep * pageDiffArr[1]
+    if (pageDiffArr[0]) deg = deg * -1
+
+    gsap.to(rangeListRef.current, {
+      rotation: `+=${deg}`,
+      onUpdate: () => setActivePage(datePage),
+    })
+
+    setRoundRotate(roundRotate + deg)
+  }, [datePage])
+
   return (
     <>
       {dates.length > 0 && (
         <section className={`${styles['date-range-wrapper']} container`}>
           <div className={styles['date-range']}>
-            <span className={styles['date-range__start']}>2015</span>
-            <span className={styles['date-range__end']}>2022</span>
+            <span className={styles['date-range__start']} ref={dateStartRef}>
+              {dateRangeStart}
+            </span>
+            <span className={styles['date-range__end']} ref={dateEndRef}>
+              {dateRangeEnd}
+            </span>
           </div>
 
           <div className={styles['range-list-wrapper']}>
-            <div className={styles['range-list']}></div>
+            <div className={styles['range-list']} ref={rangeListRef}>
+              {datePages.map((page, i) => (
+                <div
+                  key={i}
+                  className={styles['range-list__button-wrapper']}
+                  style={{ rotate: `${(i * 360) / datePages.length}deg` }}
+                >
+                  <button
+                    className={`${styles['range-list__button']} ${
+                      datePage === page ? styles['range-list__button--active'] : ''
+                    } btn btn-round`}
+                    onClick={() => setDatePage(page)}
+                  >
+                    <div
+                      style={{ rotate: `${360 - (i * 360) / datePages.length - roundRotate}deg` }}
+                      className={styles['range-list__button-desc']}
+                    >
+                      <span className={styles['range-list__button-number']}>{page + 1}</span>
+                      <span className={styles['range-list__button-title']}>{dates[page].title}</span>
+                    </div>
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       )}
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,19 @@
 export function getPageNumberView(page: number): string {
   return String(page).padStart(2, '0')
 }
+
+export function findDistanceOfArrayItems(array: number[], startItem: number, endItem: number): [boolean, number] {
+  let sortedItemIndexes = [array.indexOf(startItem), array.indexOf(endItem)].sort((a, b) => a - b)
+  let isForward = true
+
+  const remainderArray = array.slice()
+
+  const forwardLength = remainderArray.slice(sortedItemIndexes[0], sortedItemIndexes[1]).length
+  remainderArray.splice(0, forwardLength)
+  const remainderLength = remainderArray.length
+
+  if (endItem > startItem) isForward = false
+  if (forwardLength > remainderLength) isForward = !isForward
+
+  return [!isForward, Math.min(forwardLength, remainderLength)]
+}
